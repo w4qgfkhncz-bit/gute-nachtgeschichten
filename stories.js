c6106185e5f74e73c6c5f782a13bcf73859e354a
w4qgfkhncz-bit <w4qgfkhncz@privaterelay.appleid.com>
2026-07-06 13:49:25 +0200

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,17 @@
+const stories = [
+  {
+    label: "Geschichte 1",
+    title: "Der kleine Fuchs",
+    audio: "audio/geschichte-1.mp3"
+  },
+  {
+    label: "Geschichte 2",
+    title: "Der Mond über Stuttgart",
+    audio: "audio/geschichte-2.mp3"
+  },
+  {
+    label: "Geschichte 3",
+    title: "Der mutige Igel",
+    audio: "audio/geschichte-3.mp3"
+  }
+];
